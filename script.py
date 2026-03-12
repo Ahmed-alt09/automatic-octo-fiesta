a2952d25e8f6508ba955e8e248f093f8738d4ef2
Ahmed-alt09 <jerrylerry789@gmail.com>
2026-03-12 11:49:43 +0100

    Update script.py

--- script.py
+++ script.py
@@ -7,8 +7,8 @@ API_URL = "https://api.wordpress.org/plugins/info/1.2/"
 PER_PAGE = 1000
 OUTPUT_DIR = "plugins"
 
-MIN_INSTALLS = 100
-MAX_INSTALLS = 1000000
+MIN_INSTALLS = 1000
+MAX_INSTALLS = 6000
 MAX_WORKERS = 30
 
 def fetch_plugins():
